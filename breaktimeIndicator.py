7e41134981d0fad8e590f6c9704ada1985a134dd
Nishanth Vijayan <nishanththegr8@gmail.com>
2014-11-21 01:57:29 +0530

    fixed commets,indicator name,added reset,image path

--- breaktimeIndicator.py
+++ breaktimeIndicator.py
@@ -1,5 +1,12 @@
+# made by following https://bitbucket.org/cpbotha/indicator-cpuspeed
 
+# To-Add:
+#	1. Bubble notifications	
+#	2. monitor idleness
+#	3. reset after logout,lock,suspend ?
+#	4. option to change break time (currently 1Hr) 
 
+import os
 from gi.repository import Gtk, GLib
 
 try: 
@@ -7,42 +14,53 @@ try:
 except:  
        from gi.repository import AppIndicator
 
-import re
+
+MINUTES_FOR_BREAK = 60
 
 class TakeBreak:
     def __init__(self):
+
         # param1: identifier of this indicator
         # param2: name of icon. this will be searched for in the standard dirs
-        # finally, the category. We're monitoring CPUs, so HARDWARE.
-        #"/usr/share/unity/icons/launcher_icon_glow_62.png",
+        # param3: finally, the category of the indicator.
         self.ind = AppIndicator.Indicator.new(
-                            "indicator-cpuspeed",
-                            "/home/nishanth/Desktop/Invisible.png",
-                            AppIndicator.IndicatorCategory.HARDWARE)
+                            "indicator-takebreak",
+                            os.path.dirname(os.path.realpath(__file__)) + "/Invisible.png",
+                            AppIndicator.IndicatorCategory.SYSTEM_SERVICES)
 
-        # some more information about the AppIndicator:
-        # http://developer.ubuntu.com/api/ubuntu-12.04/python/AppIndicator3-0.1.html
-        # http://developer.ubuntu.com/resources/technologies/application-indicators/
+        
         self.minutes = 0
+
         # need to set this for indicator to be shown
         self.ind.set_status (AppIndicator.IndicatorStatus.ACTIVE)
         
-        # have to give indicator a menu
+        
         self.menu = Gtk.Menu()
 
-        # this is for exiting the app
+        #menu item for reseting the minutes.
+        item = Gtk.MenuItem()
+        item.set_label("Reset")
+        item.connect("activate", self.handler_menu_reset )
+        item.show()
+        self.menu.append(item)
+
+        # menu item for quiting the indicator
         item = Gtk.MenuItem()
         item.set_label("Exit                      ")
         item.connect("activate", self.handler_menu_exit	)
         item.show()
         self.menu.append(item)
 
+
+
         self.menu.show()
         self.ind.set_menu(self.menu)
-        	
-        # initialize cpu speed display
+    
+
+        # initialize time display with 0 min.
         self.update_time()
-        # then start updating every 2 seconds
+
+        # then start updating every 60 seconds
         # http://developer.gnome.org/pygobject/stable/glib-functions.html#function-glib--timeout-add-seconds
         GLib.timeout_add_seconds(60, self.handler_timeout)
 
@@ -51,20 +69,28 @@ class TakeBreak:
     def handler_menu_exit(self, evt):
         Gtk.main_quit()
 
+    def handler_menu_reset(self,evt):
+    	self.minutes = 0
+    	self.update_time()
+
 
     def handler_timeout(self):
-        """This will be called every few seconds by the GLib.timeout.
-        """
+        """	This will be called every minute by the GLib.timeout. 	"""
         self.minutes += 1
         self.update_time()
         return True
 
-    
+
+    #Note: Here the second parameter is a label_guide
+    #label_guide: An optional string to provide guidance to the panel on how big the AppIndicator label string could get.
+    #			   If this is set correctly then the panel should never 'jiggle' as the string adjusts through out the range of options.
     def update_time(self):
         self.ind.set_label(str(self.minutes)+"m","101m")
 
     def main(self):
         Gtk.main()
+
+
 
 if __name__ == "__main__":
     ind = TakeBreak()
